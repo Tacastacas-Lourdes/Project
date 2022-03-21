8dbc4852216d0f9a29fd051f051d191fbfcd204a
DESKTOP-IA8RALO\Laptop-051 <lourdes.ustp.fligno@gmail.com>
2022-03-21 11:32:36 +0800

    Adding all my files

---
+++ database/migrations/2022_03_18_074045_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('employees_name');
+            $table->string('employees_email')->unique();
+            $table->timestamp('employ_email_verified_at')->nullable();
+            $table->string('password');
+            $table->enum('type', ['Admin', 'Employees', 'Guests']);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
